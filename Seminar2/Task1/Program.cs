100f48752440ec931116434bc29b4072fd41f03f
Alexander Panteleev <unddstr@gmail.com>
2023-03-13 23:17:31 +0300

    Решил задачу №10

---
+++ Seminar2/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу которая на вход принимает 3-х значное число,
+// а на выходе показывает вторую цифру этого числа
+Console.Clear();
+int abc = new Random().Next(100, 1000);
+Console.WriteLine($"Случаной сгенированное число {abc}");
+int a = abc / 100;
+int c = abc % 10;
+int b = (abc - a * 100 - c)/10;
+Console.WriteLine(b);
