17c9745eb2f8cbdf88de0c1a125fe7361fd30da3
Alexander Panteleev <unddstr@gmail.com>
2023-03-15 21:28:00 +0300

    Решил задачу 13

---
+++ Seminar2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу, которая выводит третью цифру заданного числа,
+// или сообщает, что третьей цифры нет
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+if (n < 100)
+    Console.WriteLine("Третьей цифры нет");
+else
+{
+while (n > 999 )
+n = n/ 10;
+int m = n % 10;
+Console.WriteLine(m);
+}
+
