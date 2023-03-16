12d529c434d0173a00d6fd0e2415f64e12664cb7
Alexander Panteleev <unddstr@gmail.com>
2023-03-16 23:36:40 +0300

    взять массив из текстового файла и вывести элемент в другой текстовый файл

---
+++ Seminar2/Task4/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача со звездочкой
+using System.IO;
+Console.Clear();
+
+StreamReader sr = new StreamReader("C:\\Users\\user\\Desktop\\INPUT.txt");
+string line;
+line = sr.ReadLine();
+
+int[] a;
+a = Array.ConvertAll(line.Split(), int.Parse);
+
+sr.Close();
+int maxFirstNumber = a[0];
+int maxSecondNumber = a[0];
+int maxIndex = 0;
+for (int i = 0; i < a.Length; i++)
+{
+
+    if (a[i] > maxFirstNumber)
+    {
+        maxFirstNumber = a[i]; 
+        maxIndex = i;
+    }
+
+        if (a[i] == 0)
+        break;
+}
+a[maxIndex] = -1;
+
+for (int i = 0; i < a.Length; i++)
+{
+
+    if (a[i] > maxSecondNumber)
+    {
+        maxSecondNumber = a[i]; 
+    }
+
+        if (a[i] == 0)
+        break;
+}
+StreamWriter sw = new StreamWriter("C:\\Users\\user\\Desktop\\OUTPUT.txt");
+
+sw.WriteLine(maxSecondNumber);
+
+sw.Close();
