ea1da3bc31781e7802c6800759a143533c934acf
Alexander Panteleev <unddstr@gmail.com>
2023-03-16 20:42:08 +0300

    Решил задачу N 15

--- Seminar2/Task2/Program.cs
+++ Seminar2/Task2/Program.cs
@@ -1,6 +1,5 @@
 ﻿// Написать программу, которая выводит третью цифру заданного числа,
-// или сообщает, что третьей цифры нет
-
+// или сообщает что третьей цифры нет
 Console.Clear();
 Console.Write("Введите число: ");
 int n = int.Parse(Console.ReadLine()!);
@@ -9,9 +8,8 @@ if (n < 100)
     Console.WriteLine("Третьей цифры нет");
 else
 {
-while (n > 999 )
-n = n/ 10;
-int m = n % 10;
-Console.WriteLine(m);
+    while (n > 999)
+        n = n / 10;
+    int m = n % 10;
+    Console.WriteLine(m);
 }
-
