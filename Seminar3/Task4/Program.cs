cf4afdaece3eb674fb9fb3ad94d87d4133e70187
Alexander Panteleev <unddstr@gmail.com>
2023-03-19 14:15:40 +0300

    задача с черникой добавил пропущенный if

--- Seminar3/Task4/Program.cs
+++ Seminar3/Task4/Program.cs
@@ -5,6 +5,7 @@ int max = 0;
 
 for (int i = 0; i < arr.Length - 2; i++)
 {
+    if (arr[i] + arr[i + 1] + arr[i + 2] > max)
     max = arr[i] + arr[i + 1] + arr[i + 2];
 }
 int a = arr[^2] + arr[^1] + arr[0];
