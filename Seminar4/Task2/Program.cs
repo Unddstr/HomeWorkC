e4620c02e12674d56b4d1a0fcea90ca2ac93421f
Alexander Panteleev <unddstr@gmail.com>
2023-03-23 22:52:44 +0300

    решил 27-ю задачу

---
+++ Seminar4/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (n > 0)
+{
+    sum = sum + n % 10;
+    n = n / 10;
+}
+Console.WriteLine(sum);
