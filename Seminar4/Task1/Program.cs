a34ef6e6bcc1766497531179662c8ab94fab5308
Alexander Panteleev <unddstr@gmail.com>
2023-03-23 22:23:01 +0300

    решил 25-ю задачу

---
+++ Seminar4/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать цикл, который принимает на вход 2 числа a, b и возводит число а
+// в натуральную степень b
+Console.Clear();
+Console.WriteLine("Введите число а: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine()!);
+int result = 1;
+
+for (int i = 0; i < b; i++)
+{
+result = result * a;
+}
+Console.WriteLine(result);
