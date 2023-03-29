d5d325b46de54d6dcef719d315b833e980dd952a
Alexander Panteleev <unddstr@gmail.com>
2023-03-30 00:49:40 +0300

    Решение 43-й задачи, добавил if

--- Seminar6/Task2/Program.cs
+++ Seminar6/Task2/Program.cs
@@ -2,20 +2,25 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
 Console.Clear();
-Console.WriteLine ("Введите число: b1 = ");
+Console.Write("Введите число: b1 = ");
 int b1 = int.Parse(Console.ReadLine()!);
-Console.WriteLine ("Введите число: k1 = ");
+Console.Write("Введите число: k1 = ");
 int k1 = int.Parse(Console.ReadLine()!);
-Console.WriteLine ("Введите число: b2 = ");
+Console.Write("Введите число: b2 = ");
 int b2 = int.Parse(Console.ReadLine()!);
-Console.WriteLine ("Введите число: k2 = ");
+Console.Write("Введите число: k2 = ");
 int k2 = int.Parse(Console.ReadLine()!);
-
 double x;
 double y;
 
+if ( k1 == k2)
+{
+    Console.WriteLine("Прямые не пересекутся");
+}
+
+else
+{
 x = (b2-b1)/(k1-k2);
-
 y = k1 * x + b1;
-
 Console.WriteLine($"{x}, {y}");
+}
