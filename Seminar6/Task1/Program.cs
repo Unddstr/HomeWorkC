a1efc26659508adde2f8b8b063489833418bf95f
Alexander Panteleev <unddstr@gmail.com>
2023-03-29 23:51:33 +0300

    Решение 41-й задачи

---
+++ Seminar6/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41. Пользователь ввел с клавиатуры n чисел, посчитать сколько
+// введенных чисел > 0?
+Console.Clear();
+Console.WriteLine("Введите количество чисел: ");
+int n = int.Parse(Console.ReadLine()!);
+int count = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int numbers = new Random().Next(-100, 100);
+    if(numbers > 0)
+    count++;
+    Console.Write(numbers + " ");
+}
+Console.WriteLine();
+Console.Write($"Количество положительных чисел = {count}");
