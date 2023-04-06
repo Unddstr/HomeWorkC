fffa456263577e8ba89580e9c9ebe77cbd30c111
Alexander Panteleev <unddstr@gmail.com>
2023-04-06 23:19:59 +0300

    решение задачи 60

---
+++ Seminar8/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2*/
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}({i}, {j}, {k}) ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
+
+Console.Clear();
+int[,,] matrix = {{{11,22},{33,44}},{{55,66},{77,88}}};
+PrintMatrix(matrix);
+Console.WriteLine();
