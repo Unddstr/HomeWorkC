8deb9999c160fa55ff494548efbec62c8b710d1f
Alexander Panteleev <unddstr@gmail.com>
2023-04-07 16:15:45 +0300

    решение задачи 66

---
+++ Seminar9/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int f(int m, int n)
+{
+    if (m == n)
+        return m;
+    return f(m, n - 1) + n;
+}
+
+Console.Clear();
+Console.Write("Введите начальное число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(m, n));
