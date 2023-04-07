73e545cc7543a8cad3ebe80733a73ea8e484ad8f
Alexander Panteleev <unddstr@gmail.com>
2023-04-07 15:40:57 +0300

    решение задачи 64

---
+++ Seminar9/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿string f(int n)
+{
+    if (n == 1)
+        return "1 ";
+    return $"{n} " + f(n - 1);
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(n));
